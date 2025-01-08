ba28067539bdf248258f498bbe91fbb5815f32b1
Rahat Baksh <rahat392@gmail.com>
2025-01-08 10:04:42 +0600

    Streamlining the Navbar

--- resources/admin/components/native/Navbar.vue
+++ resources/admin/components/native/Navbar.vue
@@ -17,44 +17,6 @@ export default {
         },
     },
 };
-
-const components = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/components/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-        title: "Hover Card",
-        href: "/docs/components/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-    },
-    {
-        title: "Progress",
-        href: "/docs/components/progress",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-        title: "Scroll-area",
-        href: "/docs/components/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/components/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/components/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-];
 </script>
 
 <template>
@@ -72,7 +34,7 @@ const components = [
                     to="/"
                     class="text-sm font-medium transition-colors hover:text-primary"
                 >
-                    Overview
+                    Feed list
                 </router-link>
                 <router-link
                     to="/settings"
